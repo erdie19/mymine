571e632d7953de6d27d6bca98b3eb4864be9a39c
erdie19 <144346343+erdie19@users.noreply.github.com>
2024-07-28 04:56:47 +0200

    Update Mine.Code.js

--- Mine.Code.js
+++ Mine.Code.js
@@ -166,6 +166,10 @@ function RefreshMainFrame()
 	document.getElementById("playground").replaceChild(oMainFrame, oldFrame);
 }
 
+
+
+
+
 //Smile face button
 function FaceButton()
 {
@@ -220,6 +224,9 @@ function FaceButton()
 
 	return oButtonContainer;
 }
+
+
+
 
 //Expand the main frame after game over
 function ExpandAll()
@@ -519,9 +526,6 @@ function MineButton(mine_value,mine_index)
 	oMine.setAttribute("detected", false);
 
 	//oMine.innerText = mine_value;
-    oMine.addEventListener('touchstart', handleTouchStart, false);
-    oMine.addEventListener('touchend', handleTouchEnd, false);
-    oMine.addEventListener('touchcancel', handleTouchCancel, false);
 
 	//left mouse button response to onmouseup event, right mouse button response to onmousedown event
 	with(oMine)
@@ -611,73 +615,6 @@ function MineButton(mine_value,mine_index)
 					}
 				}
 			}
-			
-			// Codice per gestire le pressioni prolungate sui dispositivi touch
-			var longPressDuration = 500; // Durata della pressione prolungata in millisecondi
-			var longPressTimer;
-
-			function handleTouchStart(event) {
-				longPressTimer = setTimeout(function() {
-					onLongPress(event.target);
-				}, longPressDuration);
-			}
-
-			function handleTouchEnd(event) {
-				clearTimeout(longPressTimer);
-			}
-
-			function handleTouchCancel(event) {
-				clearTimeout(longPressTimer);
-			}
-
-			function onLongPress(target) {
-				// Qui va il codice per gestire la pressione prolungata
-				console.log("Long press detected on", target);
-				// Simulare il comportamento del click destro
-				if (is_end) return;
-
-				var expanded = target.getAttribute("expanded");
-				if (expanded === K_FALSE) {
-					var detected = target.getAttribute("detected");
-					var marked = target.getAttribute("marked");
-					target.className = "mine_up";
-					if (marked === K_FALSE) {
-						if (detected === K_TRUE) {
-							target.setAttribute("detected", false);
-							target.innerText = "";
-							return;
-						}
-
-						var oFlag = document.createElement("img");
-						oFlag.style.width = "15px";
-						oFlag.style.height = "15px";
-						oFlag.style.padding = "0px";
-						oFlag.style.margin = "0px";
-						oFlag.src = "images/flag.gif";
-						target.appendChild(oFlag);
-						target.setAttribute("marked", true);
-
-						rest_mine--;
-						oLeftBox.innerText = rest_mine.toString();
-
-						CheckGameStatus();
-					} else {
-						rest_mine++;
-						oLeftBox.innerText = rest_mine.toString();
-						target.removeChild(target.firstChild);
-						target.setAttribute("marked", false);
-
-						if (detected === K_FALSE) {
-							target.setAttribute("detected", true);
-							target.innerText = "?";
-							target.className = "mine_up";
-						} else {
-							target.setAttribute("detected", false);
-						}
-					}
-				}
-			}
-
 			
 			//middle mouse button, onmousedown only change the visual style of the blocks,
 			//in onmouseup the real action is taken.
@@ -971,6 +908,8 @@ function MineButton(mine_value,mine_index)
 
 	return oMine;
 }
+
+
 
 function FunctionBar(mine_num)
 {
